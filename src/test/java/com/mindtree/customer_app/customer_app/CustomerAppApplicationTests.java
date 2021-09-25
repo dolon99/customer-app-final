fcf214bfbc5d08e29df6a9a5689b2a1feaff03ab
dolon99 <dolon96749@gmail.com>
2021-09-25 09:58:44 +0530

    Program for comprehensive project

---
+++ src/test/java/com/mindtree/customer_app/customer_app/CustomerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.customer_app.customer_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
